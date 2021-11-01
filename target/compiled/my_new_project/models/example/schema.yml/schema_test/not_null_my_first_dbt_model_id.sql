4ef0c0a1cae4dbe1e68dcde2f88220f28eb42b23
Maja Ferle <maja_ferle@yahoo.com>
2021-11-01 09:53:40 +0000

    dim_customers initial commit

---
+++ target/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from raw.dbt_mferle.my_first_dbt_model
+where id is null
+
+
---
+++ target/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from raw.dbt_mferle.my_second_dbt_model
+where id is null
+
+
